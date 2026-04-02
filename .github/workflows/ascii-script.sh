474e2c6bf6fe5ea2c4ea294f9ff96d31c7c6b77a
mouhamadougueyetechniir <mouhamadou.gueye.pro@gmail.com>
2026-04-02 20:25:50 +0200

    Run  workflows with script

---
+++ .github/workflows/ascii-script.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+echo "Install Cowsay Program"
+sudo apt-get install cowsay -y
+
+echo "Execute Cowsay CMD
+cowsay -f dragon "Run for cover, I am a DRAGON...RAWR" >> dragon.txt
+
+echo "Test File Exists"
+grep -i "dragon" dragon.txt
+
+echo "Read File"
+cat dragon.txt
+
+echo "List Repo Files"
+ls -lrtha
